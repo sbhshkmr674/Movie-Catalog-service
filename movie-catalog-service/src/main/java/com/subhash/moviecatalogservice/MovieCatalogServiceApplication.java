f2da8c80e01f704d7df515731f5d43c720f6dfbc
hp <hp@host.docker.internal>
2023-05-27 20:17:43 +0530

    1st commit..

---
+++ movie-catalog-service/src/main/java/com/subhash/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -0,0 +1,14 @@
+package com.subhash.moviecatalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieCatalogServiceApplication.class, args);
+		System.out.println("1st commit..");
+	}
+
+}
