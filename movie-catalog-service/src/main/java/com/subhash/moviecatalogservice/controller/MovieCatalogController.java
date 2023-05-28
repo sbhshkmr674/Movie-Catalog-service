c7df4792dc2c5f981a200948d18e9b7d194b07d0
sbhshkmr674 <jietss2011@gmail.com>
2023-05-29 00:58:44 +0530

    Adding one more file

---
+++ movie-catalog-service/src/main/java/com/subhash/moviecatalogservice/controller/MovieCatalogController.java
@@ -0,0 +1,14 @@
+package com.subhash.moviecatalogservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/catalog")
+public class MovieCatalogController {
+	
+	@Autowired
+	private RestTemplate theRestTemplate;
+}
